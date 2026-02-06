c5f89a116086df726c33529d988a720d189b62f0
Beatriz Castilho <beatriz.c.castilho@aluno.senai.br>
2026-02-06 09:22:19 -0300

    arquivo informacoes

---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,12 @@
+const curso = "Tecnico em desenvolvimento de sistemas"
+const ano = 2025
+const notaMinimaParaSerAprovado = 50
+const notaParaSerReprovado = 49.9
+const cursoTecnico = true
+
+console.log('Minhas informacoes academicas')
+console.log("Curso:  " + curso)
+console.log(`Ano: ${ano}`)
+console.log(`Nota para a aprovacao ${notaMinimaParaSerAprovado}`)
+console.log(`Nota minima para ser reprovado ${notaParaSerReprovado}`)
+console.log(`é o curso tecnico: ${cursoTecnico}`)
