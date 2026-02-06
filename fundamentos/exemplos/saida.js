af11c1087da2967f6a64e4a15b099b280f6cc545
Beatriz Castilho <beatriz.c.castilho@aluno.senai.br>
2026-02-06 09:51:02 -0300

    arquivo saida

---
+++ fundamentos/exemplos/saida.js
@@ -0,0 +1,8 @@
+const nome = prompt("Qual seu primeiro nome?")
+const endereco = prompt("qual seu endereco?")
+const idade = prompt("qual sua idade?")
+
+alert(`
+    ${nome}, voce tem ${idade} anos,
+     \n mora no endereco ${endereco}
+    `)
